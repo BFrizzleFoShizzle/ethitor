a19921ef7d5fe8b134d3c26a612b5621bb6b646b
Bryden Frizzell <bryden.frizzell@gmail.com>
2014-12-27 20:38:18 +1300

    cleaned things up a bit

--- test.c
+++ test.c
@@ -15,56 +15,20 @@ int sock_raw1, sock_raw2;
 pthread_t thread1, thread2;
 
 void *recieveFromSocket(void*);
+void bindToDevice(int, const char[]);
 
 int getOtherSocket(int);
 
 int main()
 {
 	printf("starting\n");
-	unsigned char *pBuffer = (unsigned char *) malloc(0xFFFF);
 	//create raw sockets
 	sock_raw1 = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 	sock_raw2 = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-	//specify ethernet device
-	struct sockaddr_ll device;
-	struct ifreq ifr;
 	
-	bzero(&ifr, sizeof(ifr));
-
-	strcpy(ifr.ifr_name,"eth0\x00");
+	bindToDevice(sock_raw1, "eth0\x00");
+	bindToDevice(sock_raw2, "eth1\x00");
 	
-	printf("%s\n",ifr.ifr_name);
-	
-	printf("%i\n",ifr.ifr_ifindex);
-
-	if(ioctl(sock_raw1, SIOCGIFINDEX, &ifr)<0)
-	{
-		printf("failiure getting eth0 index\n");
-	}
-	device.sll_family = AF_PACKET;
-	device.sll_ifindex = ifr.ifr_ifindex;
-	//device.sll_protocol = htons(ETH_P_ALL);
-	
-	if(bind(sock_raw1, (struct sockaddr *) &device, sizeof(device)) == -1)
-	{
-		printf("error binding to device %i (%s)\n", device.sll_ifindex, ifr.ifr_name);
-		//printf("%s\n",explain_bind(sock_raw1, (struct sockaddr*) &device, sizeof(device)));
-	}
-	bzero(&ifr, sizeof(ifr));	
-	strcpy(ifr.ifr_name,"eth1\x00");
-	if(ioctl(sock_raw2, SIOCGIFINDEX, &ifr)<0)
-	{
-		printf("failiure getting eth1 index\n");
-	}
-	
-	device.sll_family = AF_PACKET;
-	device.sll_ifindex = ifr.ifr_ifindex;
-
-	if(bind(sock_raw2, (struct sockaddr *) &device, sizeof(device)) < 0)
-	{
-		printf("error binding to device %i (%s)\n", device.sll_ifindex, ifr.ifr_name);
-	}
-	//setsockopt(sock_raw2, SOL_SOCKET, SO_BINDTODEVICE, "eth1", 4);
 	
 	if(sock_raw1 > -1 && sock_raw2 > -1)
 	{
@@ -75,25 +39,45 @@ int main()
 		printf("Error mounting socket, %i\n", sock_raw1);
 		return 1;
 	}
-	printf("test\n");
-	
+
 	pthread_create(&thread1, NULL, recieveFromSocket, (void*)sock_raw1);
-	pthread_create(&thread2, NULL, recieveFromSocket, (void*)sock_raw2);	
-	printf("test\n");
+	pthread_create(&thread2, NULL, recieveFromSocket, (void*)sock_raw2);
+
 	while(1)
 	{
 		sleep(1000);
 		printf("listening...\n");
 	}
-	/*
-	while(1)
-	{
-		int data_size = recv(sock_raw1, pBuffer, 0xFFFF, 0);
-		printf("Packet recieved, %i bytes\n", data_size);
-	}
-	*/
-
 	return 0;
+}
+
+void bindToDevice(int sock, const char name[]) 
+{
+	//specify ethernet device
+	struct sockaddr_ll device;
+	struct ifreq ifr;
+	
+	bzero(&ifr, sizeof(ifr));
+
+	strcpy(ifr.ifr_name,name);
+	
+	printf("%s\n",ifr.ifr_name);
+	
+	printf("%i\n",ifr.ifr_ifindex);
+
+	if(ioctl(sock, SIOCGIFINDEX, &ifr)<0)
+	{
+		printf("failiure getting eth0 index\n");
+	}
+	device.sll_family = AF_PACKET;
+	device.sll_ifindex = ifr.ifr_ifindex;
+	//device.sll_protocol = htons(ETH_P_ALL);
+	
+	if(bind(sock, (struct sockaddr *) &device, sizeof(device)) == -1)
+	{
+		printf("error binding to device %i (%s)\n", device.sll_ifindex, ifr.ifr_name);
+		//printf("%s\n",explain_bind(sock_raw1, (struct sockaddr*) &device, sizeof(device)));
+	}
 }
 
 void *recieveFromSocket(void* socket) 
